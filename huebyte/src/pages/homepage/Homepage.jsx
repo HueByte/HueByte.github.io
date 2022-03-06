f892b1d4464c61a07a4176c41690100723fe7cc5
HueByte <ainodeve@gmail.com>
2022-03-06 18:06:22 +0100

    Changed text size for whoami

--- huebyte/src/pages/homepage/Homepage.jsx
+++ huebyte/src/pages/homepage/Homepage.jsx
@@ -73,21 +73,21 @@ const HomePage = () => {
               <h1>whoami</h1>
             </div>
             <div className="text">
-              <p>
+              <h3>
                 <strong>
                   Hello! On the internet I appear under the name Hue, as for now
                   20 years old professional Fullstack developer && Computer
                   Science student.
                 </strong>
                 <br />
-              </p>
-              <p>
+              </h3>
+              <h3>
                 I'm passionate about coding and though I consider .NET as my
                 main technology I'm always open for learning and mastering new
                 ones!
                 <br />
                 My future goal is to work with Artificial Intelligence research.
-              </p>
+              </h3>
             </div>
           </div>
         </div>
