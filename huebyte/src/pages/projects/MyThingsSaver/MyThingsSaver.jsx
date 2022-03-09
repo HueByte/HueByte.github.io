712f2f4fcb5b5f4c14dff9039b06b70e3552483b
HueByte <ainodeve@gmail.com>
2022-03-09 20:58:12 +0100

    Basic idea for projects layout

--- huebyte/src/pages/projects/MyThingsSaver/MyThingsSaver.jsx
+++ huebyte/src/pages/projects/MyThingsSaver/MyThingsSaver.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Spacer from "../../../core/Spacers/Spacer";
+import layer from "../../../assets/layer3.svg";
+import "../projects.scss";
 
 const MyThingsSaver = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="project-container">
+      <section>
+        <div className="intro">
+          <div className="description">
+            <h1 className="title">MyThingsSaver</h1>
+            <div>Date 2020</div>
+            <div>Web / self hosted</div>
+          </div>
+          <div className="icon"></div>
+        </div>
+      </section>
+      <Spacer layerSvg={layer} />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reiciendis
+        officiis totam animi temporibus eligendi, est ducimus expedita eos
+        accusantium quisquam harum obcaecati dolorem ex cupiditate quia id
+        explicabo ab.
+      </div>
+      <div>Technologies</div>
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores omnis
+        ratione illo esse nulla libero, aspernatur et voluptatum nostrum odit
+        fugiat vero placeat numquam nobis earum quisquam alias labore sed?
+      </div>
+      <div>MyThingsSaver</div>
+    </div>
+  );
 };
 
 export default MyThingsSaver;
