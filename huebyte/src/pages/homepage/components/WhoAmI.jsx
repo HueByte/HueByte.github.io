091fb181dd4e984ed69711a70be3f22dfd163590
HueByte <ainodeve@gmail.com>
2022-03-10 00:16:09 +0100

    lighthouse improvements

--- huebyte/src/pages/homepage/components/WhoAmI.jsx
+++ huebyte/src/pages/homepage/components/WhoAmI.jsx
@@ -9,20 +9,20 @@ const WhoAmI = () => {
             <h1>whoami</h1>
           </div>
           <div className="text">
-            <h3>
+            <h2>
               <strong>
                 Hello! On the internet I appear under the name Hue, as for now
                 20 years old professional Fullstack developer && Computer
                 Science student.
               </strong>
               <br />
-            </h3>
-            <h3>
+            </h2>
+            <h2>
               I'm passionate about coding and though I consider .NET as my main
               technology I'm always open for learning and mastering new ones!
               <br />
               My future goal is to work with Artificial Intelligence research.
-            </h3>
+            </h2>
           </div>
         </div>
       </div>
