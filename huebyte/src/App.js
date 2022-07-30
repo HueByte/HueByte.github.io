aa735caa72e0b802c0703d87b8021dba262e1f65
HueByte <ainodeve@gmail.com>
2022-07-30 20:32:26 +0200

    added custom cursor

--- huebyte/src/App.js
+++ huebyte/src/App.js
@@ -6,16 +6,43 @@ import Loader from "./core/loader/Loader";
 import "./App.scss";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
   const history = createBrowserHistory();
   const [isLoading, setIsLoading] = useState(true);
+  const cursor = useRef();
 
   useEffect(() => {
     const preloadedImages = ["https://github.com/huebyte.png"];
 
     cacheImages(preloadedImages);
+
+    cursor.current = document.querySelector(".cursor");
+
+    document.addEventListener("mousemove", cursorMove);
+
+    document.addEventListener("click", cursorClick);
+
+    return () => {
+      document.removeEventListener("mousemove", cursorMove);
+      document.removeEventListener("click", cursorClick);
+    };
   }, []);
+
+  const cursorMove = (event) => {
+    cursor.current.setAttribute(
+      "style",
+      "top: " + (event.pageY - 10) + "px; left: " + (event.pageX - 10) + "px;"
+    );
+  };
+
+  const cursorClick = () => {
+    cursor.current.classList.add("expand");
+    setTimeout(() => {
+      cursor.current.classList.remove("expand");
+    }, 500);
+  };
 
   const cacheImages = async (preloadSources) => {
     const promises = await preloadSources.map((src) => {
@@ -35,6 +62,7 @@ function App() {
     <BrowserRouter history={history}>
       <Suspense fallback={<Loader />}>
         {isLoading ? <Loader /> : <ClientRoutes />}
+        <div class="cursor"></div>
       </Suspense>
     </BrowserRouter>
   );
