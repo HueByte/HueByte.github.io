d541a3a3d403bf6ea508db4e00324546636db5b4
HueByte <ihuebyte@gmail.com>
2023-09-21 12:17:59 +0200

    Minor updates to intro UI

--- huebyte/src/pages/homepage/components/Intro.jsx
+++ huebyte/src/pages/homepage/components/Intro.jsx
@@ -5,38 +5,41 @@ import { BiMailSend } from "react-icons/bi";
 import layer1 from "../../../assets/layer1.svg";
 
 const Intro = () => {
-  return (
-    <div className="intro">
-      <div className="avatar">
-        <img src="https://github.com/huebyte.png" alt="huebyte" />
-        <a
-          href="https://discordapp.com/users/215556401467097088"
-          target="_blank"
-          className="orb-icon"
-        >
-          <FaDiscord />
-        </a>
-        <a
-          href="https://github.com/HueByte"
-          target="_blank"
-          className="orb-icon"
-        >
-          <AiFillGithub />
-        </a>
-        <a
-          href="mailto:ihuebyte@gmail.com"
-          target="_blank"
-          className="orb-icon"
-        >
-          <BiMailSend />
-        </a>
-      </div>
-      <div
-        className="intro-spacer"
-        style={{ backgroundImage: `url(${layer1}` }}
-      ></div>
-    </div>
-  );
+	return (
+		<div className="intro">
+			<div className="avatar">
+				<img src="https://github.com/huebyte.png" alt="huebyte" />
+				<a
+					href="https://discordapp.com/users/215556401467097088"
+					target="_blank"
+					className="orb-icon"
+					rel="noreferrer"
+				>
+					<FaDiscord />
+				</a>
+				<a
+					href="https://github.com/HueByte"
+					target="_blank"
+					className="orb-icon"
+					rel="noreferrer"
+				>
+					<AiFillGithub />
+				</a>
+				<a
+					href="mailto:ihuebyte@gmail.com"
+					target="_blank"
+					className="orb-icon"
+					rel="noreferrer"
+				>
+					<BiMailSend />
+				</a>
+			</div>
+			<div
+				className="intro-spacer"
+				style={{ backgroundImage: `url(${layer1}` }}
+			></div>
+		</div>
+	);
 };
 
 export default Intro;
