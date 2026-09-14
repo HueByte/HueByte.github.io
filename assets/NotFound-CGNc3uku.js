668d8f2f326f85f521e0e64ef1c57a9eeb4ae034
HueByte <HueByte@users.noreply.github.com>
2026-09-14 14:25:41 +0000

    deploy: e7d7af25bf73a0c4c4a85b103acd6e0d09c41b40

--- assets/NotFound-CGNc3uku.js
+++
@@ -1,1 +0,0 @@
-import{n as e,r as t}from"./index-CI0Y4EfJ.js";var n=e();function r(){return(0,n.jsxs)(`section`,{className:`not-found`,children:[(0,n.jsx)(`h1`,{children:`404`}),(0,n.jsx)(`p`,{children:`There is nothing here.`}),(0,n.jsx)(t,{to:`/`,children:`Back home`})]})}export{r as default};
